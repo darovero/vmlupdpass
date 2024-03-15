348d32e6e2f64f144677eda4db09eeba8b3aed6b
David Roberto Velásquez Rojas <drvelasquezr@outlook.com>
2024-03-15 16:14:10 +0000

    Updated check_password_expiry.ps1

--- scripts/check_password_expiry.ps1
+++ scripts/check_password_expiry.ps1
@@ -3,8 +3,8 @@ function Check-User {
     param(
         [string]$Username
     )
-    $ResourceGroupName = 'devvmlab0_group'  # Reemplaza con el nombre de tu grupo de recursos
-    $VMName = 'devvmlab03 '  # Reemplaza con el nombre de tu máquina virtual
+    $ResourceGroupName = 'YourResourceGroup'  # Reemplaza con el nombre de tu grupo de recursos
+    $VMName = 'YourVMName'  # Reemplaza con el nombre de tu máquina virtual
     $Command = "/usr/sbin/chage -l $Username | grep 'Password expires' | awk -F': ' '{print $2}'"
     $Output = Invoke-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName $VMName -CommandId 'RunShellScript' -Script $Command
     $ExpirationDate = $Output.Value
@@ -12,7 +12,7 @@ function Check-User {
 }
 
 # Check password expiration for each user provided as argument
-$Usernames = @('darovero', 'davidv')  # Reemplaza con los nombres de usuario reales
+$Usernames = @('davidv', 'darovero')  # Reemplaza con los nombres de usuario reales
 foreach ($Username in $Usernames) {
     Check-User -Username $Username
 }
